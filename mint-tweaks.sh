5630b3e0db553fc910feb122ba0f275b3c356640
zibetnu <zibetnu@pm.me>
2025-09-06 11:59:01 -0600

    Disable bluetooth connection notifications

--- mint-tweaks.sh
+++ mint-tweaks.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-echo "This script will:
-- Make windows maximize when dragged to top of screen
+echo "Adjusts annoying default settings
+
+This script will:
+- Make windows maximize (instead of tile) when dragged to top of screen
 - Disable mouse acceleration
 - Hide the update manager tray icon when there are no updates available
+- Disable bluetooth connection notifications
 "
 read -erp "Is this okay? [y/N] "
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	gsettings set org.cinnamon.muffin tile-maximize true
 	gsettings set org.cinnamon.desktop.peripherals.mouse accel-profile flat
 	gsettings set com.linuxmint.updates hide-systray true
+	gsettings set org.blueman.general plugin-list "['\!ConnectionNotifier']"
 	echo "Done!"
 else
 	echo "No changes were made."
