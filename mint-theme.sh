bfa89cbdf439e1309fb8476caec47c13746ee518
zibetnu <zibetnu@pm.me>
2025-09-06 11:39:57 -0600

    Set more theme settings

--- mint-theme.sh
+++ mint-theme.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-echo "This script will:
-- Download Papirus icons
-- Set the theme to Mint-Y-Dark
-- Set the theme icons to Papirus-Dark
-- Set the folder color to green
+echo "Configures dark theme with modern icons and green accent
+
+This script will:
+- Install the Papirus icon theme
+- Set the Papirus-Dark folder color to green
+- Set theme settings:
+  Mouse Pointer : Bibata-Modern-Classic
+  Applications  : Mint-Y-Dark
+  Icons         : Papirus-Dark
+  Desktop       : Mint-Y-Dark
 
 You will be asked for your password.
 "
@@ -14,12 +19,15 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	sudo apt -y update
 	sudo apt -y install papirus-icon-theme
 	sudo apt -y install papirus-folders
+	sudo apt autoremove
 
+	papirus-folders --color green --theme Papirus-Dark
+
+	gsettings set org.cinnamon.desktop.interface cursor-theme Bibata-Modern-Classic
 	gsettings set org.cinnamon.desktop.interface gtk-theme Mint-Y-Dark
 	gsettings set org.cinnamon.desktop.interface icon-theme Papirus-Dark
-	papirus-folders -C green --theme Papirus-Dark
+	gsettings set org.cinnamon.theme name Mint-Y-Dark
 
-	sudo apt autoremove
 	echo "Done!"
 else
 	echo "No changes were made."
